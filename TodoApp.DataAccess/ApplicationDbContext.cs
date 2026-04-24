28b724fdc61eef8912328d80f457e0432ae3fea0
Danylo Bezdolnyi <b.danil2007@gmail.com>
2026-04-24 16:24:48 +0300

    Add ApplicationDbContext

---
+++ TodoApp.DataAccess/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TodoApp.DataAccess
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; } = null!;
+
+        public DbSet<TaskItem> TaskItems { get; set; } = null!;
+
+        public DbSet<Category> Categories { get; set; } = null!;
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
