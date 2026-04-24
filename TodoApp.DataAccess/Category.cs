4ccecbc7456281d08bd80a4a7523cdc41936b75e
Danylo Bezdolnyi <b.danil2007@gmail.com>
2026-04-24 15:46:35 +0300

    Add Category entity

--- TodoApp.DataAccess/Category.cs
+++ TodoApp.DataAccess/Category.cs
@@ -1,12 +1,11 @@
-﻿    using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TodoApp.DataAccess
+﻿namespace TodoApp.DataAccess
 {
     public class Category
     {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public int UserId { get; set; }
+        public User User { get; set; } = null!;
+        public ICollection<TaskItem> Tasks { get; set; } = new List<TaskItem>();
     }
 }
