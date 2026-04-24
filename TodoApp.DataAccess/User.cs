2e6cb89d94f569246ecef3bc6d22d18292883e91
Danylo Bezdolnyi <b.danil2007@gmail.com>
2026-04-24 15:33:38 +0300

    Add User entity

---
+++ TodoApp.DataAccess/User.cs
@@ -0,0 +1,14 @@
+﻿namespace TodoApp.DataAccess
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+        public string Email { get; set; } = null!;
+        public string PasswordHash { get; set; } = null!;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public ICollection<TaskItem> Tasks { get; set; } = new List<TaskItem>();
+        public ICollection<Category> Categories { get; set; } = new List<Category>();
+    }
+}
