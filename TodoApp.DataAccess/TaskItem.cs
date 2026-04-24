e38dab672662ad0915fcf3ce84227bebd61e51ba
Danylo Bezdolnyi <b.danil2007@gmail.com>
2026-04-24 15:43:41 +0300

    Add TaskItem entity

--- TodoApp.DataAccess/TaskItem.cs
+++ TodoApp.DataAccess/TaskItem.cs
@@ -1,12 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TodoApp.DataAccess
+﻿namespace TodoApp.DataAccess
 {
     public class TaskItem
     {
+        public int Id { get; set; }
+        public string Title { get; set; } = null!;
+        public string? Description { get; set; }
+        public bool IsCompleted { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? DueDate { get; set; }
+        public int UserId { get; set; }
+        public int CategoryId { get; set; }
+        public User User { get; set; } = null!;
+        public Category Category { get; set; } = null!;
     }
 }
