c5a8d2be80efdb9b2a7aaa74fc4fbf4c028bcc30
Danylo Bezdolnyi <b.danil2007@gmail.com>
2026-05-01 02:37:08 +0300

    Add IApplicationDbContext to abstract data access

--- TodoApp.DataAccess/ApplicationDbContext.cs
+++ TodoApp.DataAccess/ApplicationDbContext.cs
@@ -3,17 +3,16 @@ using Microsoft.EntityFrameworkCore;
 
 namespace TodoApp.DataAccess
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : DbContext, IApplicationDbContext
     {
         public DbSet<User> Users { get; set; } = null!;
-
         public DbSet<TaskItem> TaskItems { get; set; } = null!;
-
         public DbSet<Category> Categories { get; set; } = null!;
 
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
---
+++ TodoApp.Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace TodoApp.Interfaces;
+
+public interface IApplicationDbContext
+{
+    DbSet<User> Users { get; set; } 
+    DbSet<TaskItem> TaskItems { get; set; } 
+    DbSet<Category> Categories { get; set; }
+
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+}
