47101399ab85e8c31fc4f5cdc09d93a2339ef242
Danylo Bezdolnyi <b.danil2007@gmail.com>
2026-05-11 17:56:48 +0300

    implement deleteTask method, login component

--- src/frontend/src/app/pages/login/login.ts
+++ src/frontend/src/app/pages/login/login.ts
@@ -1,9 +1,38 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {}
+export class Login {
+  email=""
+  password=""
+
+  constructor(private http: HttpClient) {}
+
+  logIn() {
+    const body = {
+      email: this.email,
+      password: this.password
+    }
+
+    this.http.post(
+    'https://localhost:7178/api/auth/login',
+    body
+    )
+    .subscribe({
+      next: (response) => {
+        console.log("SUCCESS");
+        console.log(response);
+      },
+      error: (error) => {
+        console.log("ERROR");
+        console.log(error);
+      }
+    })
+  }
+}
