4ba8559b2fc675b40d48e383a99b041895153bb4
Danylo Bezdolnyi <b.danil2007@gmail.com>
2026-05-11 04:32:49 +0300

    implement navbar and tasks component

--- src/frontend/src/app/pages/tasks/tasks.ts
+++ src/frontend/src/app/pages/tasks/tasks.ts
@@ -1,9 +1,39 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+export interface Todo {
+  isCompleted: boolean;
+  taskTitle: string;
+  taskDescription: string | null;
+}
 
 @Component({
   selector: 'app-tasks',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './tasks.html',
   styleUrl: './tasks.css',
 })
-export class Tasks {}
+
+export class Tasks {
+  tasks: Todo[] = 
+  [
+    { isCompleted: false, taskTitle: "Learn Angular", taskDescription: "Finish Todo frontend MPV"},
+  ]
+
+  isCompleted = false
+  taskTitle =""
+  taskDescription=""
+
+  addTask() {
+    const todo =
+      {
+        isCompleted: this.isCompleted, 
+        taskTitle: this.taskTitle, 
+        taskDescription: this.taskDescription
+      }
+
+    this.tasks.push(todo)
+    this.taskTitle=""
+    this.taskDescription=""
+  }
+}
